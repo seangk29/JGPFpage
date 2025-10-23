479940b4cd35686b47af2a82e54e2708033df850
seangk29 <seangk29@gmail.com>
2025-10-23 16:39:14 +0100

    images

--- public/Week3/assetsl/public/javaScript/task12.js
+++ public/Week3/assetsl/public/javaScript/task12.js
@@ -6,7 +6,7 @@ var faces = [6]
 
 function preLoad()
 {
-faces[0] = new Image(); faces[0].src ="images/dice-1.png"; 
+faces[0] = new Image(); faces[0].src ="images/images/dice-1.png"; 
 faces[1] = new Image(); faces[0].src ="images/dice-2.png"; 
 faces[2] = new Image(); faces[0].src ="images/dice-3.png"; 
 faces[3] = new Image(); faces[0].src ="images/dice-4.png"; 
